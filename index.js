97b25992cc5c5a55eae6e6cab73c9cabbfe0fdd2
Piyush kushwah <piyushkushwah002@gmail.com>
2026-02-26 14:35:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+// console.log("Hello javascript");
+
+
+// a= 10
+
+//  console.log(a);
+
+ // var let const
+
+//  var a = 10 
+//  var a = 20
+//  console.log(a);
+
+//  let a = 10
+//   a = 20
+//  console.log(a);
+
+// const a = 10
+//  a = 20
+// console.log(a);
+
+// var a = 10
+// let b= 20
+// const c = 30
+
+// function abc(){
+//     console.log(a);
+//      console.log(b);
+//       console.log(c);
+// }
+
+// if(true){
+//     var a = 10
+//     let b = 20
+//     const c = 30
+// }
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a);
+// console.log(b);
+
+
+// var a = 10
+// let b = 20
+// const c = 30
+
+// abc()
+
+// function abc(){
+
+//     var a = 10
+//     let b = 20
+//     const c = 30
+
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
