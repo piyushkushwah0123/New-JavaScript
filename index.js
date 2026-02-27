349fcb9fb0fff5e0f4d5c0b74eb9f96ed58ccb0c
Piyush kushwah <piyushkushwah002@gmail.com>
2026-02-27 12:01:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -23,11 +23,14 @@
 // let b= 20
 // const c = 30
 
+// Scope :- Global, Functional , Block
+
 // function abc(){
 //     console.log(a);
 //      console.log(b);
 //       console.log(c);
 // }
+// abc()
 
 // if(true){
 //     var a = 10
@@ -67,3 +70,36 @@
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+// Data Types :- 1.Primitive , 2. Non primitive 
+
+
+// Call By reference :--
+
+// let a = "JavaScript"
+
+// let obj = {
+//     id:1,
+//     name:"ramesh",
+//     role:"Student",
+// }
+
+
+// let obj1 = obj
+
+// obj1.name ="suresh"
+
+// console.log(obj);
+// console.log(obj1);
+
+
+// Call By  value:--
+
+function abc(x){
+     x = 2 
+  console.log(x);
+}
+let a= 10
+
+abc(a)            // Argument :- jab bhi call krte h , actual value pass krte hai 
+         
